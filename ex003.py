3ef61799566ce021156dbf7c6576c03ec3b8509b
KillersSeven <89374153+KillersSeven@users.noreply.github.com>
2022-05-18 23:12:13 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite o Primeiro Valor: '))
 n2 = int(input('Digite o Segundo Valor: '))
 soma = n1+n2
-print('A soma entre {} e {} é igual a {}'.format(n1, n2, soma))
+print(f'A soma entre {n1} e {n2} é igual a {soma}')
